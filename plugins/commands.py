2badbc9f3682955a5270328b5a272ff47d3c3a55
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-03-12 12:37:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -152,7 +152,11 @@ async def start(client, message):
                 protect_content=settings['file_secure'],
                 reply_markup=InlineKeyboardMarkup(btn)
             )
-        return
+            k = await btn.reply("<b>⚠️  ᴀꜰᴛᴇʀ 5 ᴍɪɴᴜᴛᴇꜱ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ  🗑️</b>", quote=True)
+            await asyncio.sleep(300)
+            await btn.delete()
+            await k.delete()
+            return
 
     type_, grp_id, file_id = mc.split("_", 2)
     files_ = await get_file_details(file_id)
@@ -202,9 +206,9 @@ async def start(client, message):
         protect_content=settings['file_secure'],
         reply_markup=InlineKeyboardMarkup(btn)
     )
-    k = await msg.reply("<b>⚠️  ᴀꜰᴛᴇʀ 5 ᴍɪɴᴜᴛᴇꜱ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ  🗑️</b>", quote=True)
+    k = await btn.reply("<b>⚠️  ᴀꜰᴛᴇʀ 5 ᴍɪɴᴜᴛᴇꜱ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ  🗑️</b>", quote=True)
     await asyncio.sleep(300)
-    await msg.delete()
+    await btn.delete()
     await k.delete()
 
 @Client.on_message(filters.command('index_channels') & filters.user(ADMINS))
