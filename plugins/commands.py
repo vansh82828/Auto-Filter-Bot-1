82f7a0b794625723ae24150b416c486b0095a1c0
vansh82828 <156403334+vansh82828@users.noreply.github.com>
2024-03-12 21:40:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -564,7 +564,7 @@ async def remove_premium_cmd_handler(client, message):
             await message.reply_text("Premium access removed to the user.")
             await client.send_message(
                 chat_id=user_id,
-                text=f"<b>premium removed by admins \n\n Contact Admin if this is mistake \n\n 👮 Admin : @Rk_botowner \n</b>",                
+                text=f"<b>premium removed by admins \n\n Contact Admin if this is mistake \n\n 👮 Admin : @Shivacharan7773 \n</b>",                
             )
         else:
             await message.reply_text("Invalid time format.'")
